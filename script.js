8f150863d6ffacd230e4638b76603b8d974c04e5
Connor Schotz <140295366+Packstone01@users.noreply.github.com>
2024-10-07 18:15:16 -0400

    Changed some things on the style.css such as the skills tab.

---
+++ script.js
@@ -0,0 +1,38 @@
+// function add7(num) {
+//   return num + 7;
+// }
+
+// console.log(add7(3));
+
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// console.log(multiply(2, 2));
+
+// function capitalize(string) {
+//   string = string.toLowerCase();
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+
+// console.log(capitalize('connOR'));
+
+// function lastLetter(letter) {
+//   return letter.slice(-1);
+// }
+
+// console.log(lastLetter("abcdefg"));
+
+// let answer = parseInt(prompt('Please enter a number you would like to FizzBuzz up to: '));
+
+// for (let i = 1; i <= answer; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//       console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
